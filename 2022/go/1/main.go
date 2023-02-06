38f45904b20867897c763ea211520803408d9d8c
dcubides <dcubides@twilio.com>
2023-02-06 15:22:20 -0500

    send day 1 part 2

--- 2022/go/1/main.go
+++ 2022/go/1/main.go
@@ -7,22 +7,8 @@ import (
 	"strconv"
 )
 
-func PartOne(nums []int) (int, error) {
-	last := nums[0]
-	count := 0
-
-	for _, val := range nums {
-		if val > last {
-			count += 1
-		}
-		last = val
-	}
-
-	return count, nil
-}
-
 func main() {
-	filename := "input.txt"
+	filename := "input2.txt"
 	readFile, err := os.Open(filename)
 
 	if err != nil {
@@ -30,7 +16,11 @@ func main() {
 	}
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
-	var elf = 0
+	part2(fileScanner)
+
+}
+
+func part1(fileScanner *bufio.Scanner) {
 	var maxCalories = 0
 	var calories = 0
 	for fileScanner.Scan() {
@@ -39,12 +29,38 @@ func main() {
 			if calories >= maxCalories {
 				maxCalories = calories
 			}
-			elf += 1
+			calories = 0
 			continue
 		}
 		intvalue, _ := strconv.Atoi(line)
 		calories += intvalue
 	}
-
 	fmt.Println(maxCalories)
+}
+
+func part2(fileScanner *bufio.Scanner) {
+	var elfs = []int{0, 0, 0}
+	var calories = 0
+	var elf = 0
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		if line != "" {
+			intvalue, _ := strconv.Atoi(line)
+			calories += intvalue
+			fmt.Printf("%d, ", intvalue)
+			continue
+		}
+		fmt.Println("\n", calories, elf)
+		if calories >= elfs[0] {
+			elfs[0] = calories
+		} else if calories >= elfs[1] {
+			elfs[1] = calories
+		} else if calories >= elfs[2] {
+			elfs[2] = calories
+		}
+		calories = 0
+		elf += 1
+	}
+	fmt.Printf("len=%d cap=%d %v\n", len(elfs), cap(elfs), elfs)
+	fmt.Println(elfs[0] + elfs[1] + elfs[2])
 }
