4bad4fd7eae0520a448aa38f16172458597fb466
dcubides <dcubides@twilio.com>
2023-02-09 12:51:01 -0500

    solve day 2

--- 2022/go/2/main.go
+++ 2022/go/2/main.go
@@ -9,7 +9,19 @@ import (
 )
 
 var ChoiceValue = map[string]int{"X": 1, "Y": 2, "Z": 3}
+var ResultValue = map[string]int{"Y": 3, "X": 0, "Z": 6}
 var ProcessValue = map[string]int{"X": 1, "Y": 2, "Z": 3, "A": 1, "B": 2, "C": 3}
+var decisionMap = map[string]string{
+	"AY": "X",
+	"AX": "Z",
+	"AZ": "Y",
+	"BY": "Y",
+	"BX": "X",
+	"BZ": "Z",
+	"CY": "Z",
+	"CX": "Y",
+	"CZ": "X",
+}
 
 const WinValue = 6
 const LoseValue = 0
@@ -17,15 +29,24 @@ const DrawValue = 3
 
 func main() {
 	input := readFile()
-	result := RPStournament(input)
-	fmt.Println(result)
+	result := RPStournament(input, scoreRound)
+	fmt.Printf("part 1: %d\n", result)
+	result = RPStournament(input, scoreRoundPart2)
+	fmt.Printf("part 2: %d\n", result)
 }
 
-func RPStournament(input []string) int {
+func scoreRoundPart2(opponentChoice string, result string) int {
+	yourChoice := decisionMap[fmt.Sprintf("%s", opponentChoice+result)]
+	fmt.Printf("%s - %s : %s == (%d + %d)total result %d  ", opponentChoice, yourChoice, result, ChoiceValue[yourChoice], ResultValue[result], ChoiceValue[yourChoice]+ResultValue[result])
+	return ChoiceValue[yourChoice] + ResultValue[result]
+}
+
+func RPStournament(input []string, scoreFunction func(string, string) int) int {
 	result := 0
 	for _, round := range input {
 		choices := strings.Split(round, " ")
-		result += scoreRound(choices[0], choices[1])
+		result += scoreFunction(choices[0], choices[1])
+		fmt.Println(result)
 	}
 	return result
 }
@@ -33,6 +54,7 @@ func RPStournament(input []string) int {
 func readFile() []string {
 	puzzle := make([]string, 0)
 	readFile, err := os.Open("input.txt")
+	//readFile, err := os.Open("example.txt")
 	if err != nil {
 	}
 	fileScanner := bufio.NewScanner(readFile)
