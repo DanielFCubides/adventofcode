d59856811ecefdc054689c308dfa7e7308cc07bf
DanielFCubides <dfcubidesc@gmail.com>
2023-03-02 21:01:10 -0500

    add part 2 day 3

--- 2022/go/3/main.go
+++ 2022/go/3/main.go
@@ -8,8 +8,38 @@ import (
 
 func main() {
 	input := readFile()
-	result := RuckSackReorganization(input)
+	result := RuckSackReorganization2(input)
 	fmt.Print(result)
+}
+
+func RuckSackReorganization2(input []string) int {
+	result := 0
+	size := len(input) / 3
+	for i := 0; i < size; i++ {
+		badge := GetElfBadge(input, i)
+		value := StringNumericPriority(badge)
+		result += value
+
+	}
+	return result
+}
+
+func StringNumericPriority(char string) int {
+	valueOfA := 65
+	valueOfa := 97
+	if int(char[0]) >= valueOfa {
+		return int(char[0]) + 1 - valueOfa
+	}
+	return int(char[0]) + 27 - valueOfA
+}
+
+func GetElfBadge(input []string, i int) string {
+	elf1 := i * 3
+	elf2 := elf1 + 1
+	elf3 := elf1 + 2
+	intersect1 := stringIntersection(input[elf1], input[elf2])
+	result := stringIntersection(intersect1, input[elf3])[0]
+	return string(result)
 }
 
 func RuckSackReorganization(input []string) int {
@@ -44,8 +74,8 @@ func DivideCompartment(rucksack string) (string, string) {
 
 func readFile() []string {
 	puzzle := make([]string, 0)
-	//readFile, err := os.Open("input.txt")
-	readFile, err := os.Open("example.txt")
+	readFile, err := os.Open("input.txt")
+	//readFile, err := os.Open("example.txt")
 	if err != nil {
 	}
 	fileScanner := bufio.NewScanner(readFile)
@@ -54,4 +84,22 @@ func readFile() []string {
 		puzzle = append(puzzle, fileScanner.Text())
 	}
 	return puzzle
+}
+
+func stringIntersection(slice1 string, slice2 string) string {
+	result := ""
+	slice1Map := make(map[string]int, 0)
+	for _, element := range slice1 {
+		if _, ok := slice1Map[string(element)]; !ok {
+			slice1Map[string(element)] = 1
+		}
+
+	}
+	for _, charRune := range slice2 {
+		if _, ok := slice1Map[string(charRune)]; ok {
+			result += string(charRune)
+		}
+	}
+
+	return result
 }
