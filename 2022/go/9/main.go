3933f4b8ff09b918054e6dee994d52c4f8e885cc
DanielFCubides <dfcubidesc@gmail.com>
2024-01-16 14:52:13 -0500

    Add day 9 part 2

--- 2022/go/9/main.go
+++ 2022/go/9/main.go
@@ -14,74 +14,119 @@ type position struct {
 	y int
 }
 
-func tailsPositions(movements []string) int {
-	headPositionsMap := map[position]int{position{0, 0}: 1}
-	tailPositionsMap := map[position]int{position{0, 0}: 1}
-	currentHeadPosition := position{0, 0}
-	currentTailPosition := position{0, 0}
-	for _, move := range movements {
-		simulateMovement(&headPositionsMap, &tailPositionsMap, &currentHeadPosition, &currentTailPosition, move)
-	}
-	fmt.Printf("H: %d - T: %d\n\n", len(headPositionsMap), len(tailPositionsMap))
-	//for key, _ := range tailPositionsMap {
-	//	fmt.Printf("x: %d y: %d\n ", key.x, key.y)
-	//}
-	return len(tailPositionsMap)
+func (p position) calculateDistance(position2 position) int {
+	return int(math.Sqrt(math.Pow(float64(position2.x-p.x), 2) + math.Pow(float64(position2.y-p.y), 2)))
 }
 
-func moveHead(direction string, positions map[position]int, currentPosition *position) {
+type rope struct {
+	knotPosition  map[int]*position
+	tailPositions map[string]int
+}
+
+func (r rope) move(direction string, units int) {
+	fmt.Printf("\n%s - %d | | | tail movementes: %+v \n\n ", direction, units, r.tailPositions)
+	for i := 0; i < units; i++ {
+		r.moveKnots(direction)
+	}
+}
+
+func (r rope) printPositions() {
+	for i := 0; i < len(r.knotPosition); i++ {
+		knot := r.knotPosition[i]
+		fmt.Printf("knot %d [ %d, %d]\n", i, knot.x, knot.y)
+	}
+	fmt.Println()
+}
+
+func (r rope) moveKnots(direction string) {
 	switch direction {
 	case "R":
-		currentPosition.x++
+		r.knotPosition[0].x++
 		break
 	case "L":
-		currentPosition.x--
+		r.knotPosition[0].x--
 		break
 	case "U":
-		currentPosition.y++
+		r.knotPosition[0].y++
 		break
 	case "D":
-		currentPosition.y--
+		r.knotPosition[0].y--
 		break
 	default:
 		break
 	}
-	AddPosition(positions, currentPosition)
-
-}
-
-func AddPosition(Positions map[position]int, CurrentPosition *position) {
-	if _, ok := Positions[*CurrentPosition]; !ok {
-		Positions[*CurrentPosition] = 1
-	}
-	Positions[*CurrentPosition]++
-}
-
-func simulateMovement(headPositions *map[position]int, tailPositionsMap *map[position]int, currentHeadPosition *position, currentTailPosition *position, move string) {
-	direction, units := parseMovement(move)
-	//fmt.Printf(" %s %d\n", direction, units)
-	for i := 0; i < units; i++ {
-		previousHeadPosition := *currentHeadPosition
-		moveHead(direction, *headPositions, currentHeadPosition)
-		shouldMove := TailShouldMove(*currentTailPosition, *currentHeadPosition)
-		if shouldMove {
-			moveTail(currentTailPosition, previousHeadPosition, tailPositionsMap)
+	for i := 0; i < len(r.knotPosition); i++ {
+		knot := r.knotPosition[i]
+		if i == 0 {
+			continue
 		}
-
-		//fmt.Printf("H x:%d, y:%d\n", currentHeadPosition.x, currentHeadPosition.y)
-		//fmt.Printf("T x:%d, y:%d\n", currentTailPosition.x, currentTailPosition.y)
+		knotPosition := *knot
+		fmt.Printf("%d - %+v:  ", i, knotPosition)
+		PlankMove(r.knotPosition[i-1], knot)
+		if i == len(r.knotPosition)-1 {
+			tailPosition := fmt.Sprintf("%d,%d", knot.x, knot.y)
+			if _, ok := r.tailPositions[tailPosition]; !ok {
+				r.tailPositions[tailPosition] = 1
+			}
+		}
 	}
-	//fmt.Println()
+	r.printPositions()
 }
 
-func moveTail(tail *position, head position, positions *map[position]int) {
-	tail.x = head.x
-	tail.y = head.y
-	AddPosition(*positions, tail)
+func PlankMove(head *position, tail *position) {
+	distance := head.calculateDistance(*tail)
+	//fmt.Printf("(%d, %d) head , (%d, %d) Tail, (%d, %d) hpp, (%d) distance\n",
+	//	head.x,
+	//	head.y,
+	//	tail.x,
+	//	tail.y,
+	//	previousHeadPosition.x,
+	//	previousHeadPosition.y,
+	//	distance)
+	if distance <= 1 {
+		fmt.Println()
+		return
+	}
+	fmt.Printf("  move knot from (%d,%d) ", tail.x, tail.y)
+	xMovement := 0
+	if tail.x > head.x {
+		xMovement = -1
+	}
+	if tail.x < head.x {
+		xMovement = 1
+	}
+	yMovement := 0
+	if tail.y > head.y {
+		yMovement = -1
+	}
+	if tail.y < head.y {
+		yMovement = 1
+	}
+	*tail = position{x: tail.x + xMovement, y: tail.y + yMovement}
+	fmt.Printf(" to (%d,%d)\n\n", tail.x, tail.y)
 }
 
-func TailShouldMove(tail position, head position) bool {
-	return math.Abs(float64(tail.x-head.x)) > 1 || math.Abs(float64(tail.y-head.y)) > 1
+func tailsPositions(movements []string) int {
+	rope := rope{
+		knotPosition: map[int]*position{
+			0: {0, 0},
+			1: {0, 0},
+			2: {0, 0},
+			3: {0, 0},
+			4: {0, 0},
+			5: {0, 0},
+			6: {0, 0},
+			7: {0, 0},
+			8: {0, 0},
+			9: {0, 0},
+		},
+		tailPositions: make(map[string]int, 0),
+	}
+	for _, move := range movements {
+		rope.move(parseMovement(move))
+	}
+	fmt.Print(rope.tailPositions)
+	return len(rope.tailPositions)
 }
 
 func parseMovement(move string) (string, int) {
@@ -93,6 +138,7 @@ func parseMovement(move string) (string, int) {
 
 func readFile() []string {
 	filename := "input.txt"
+
 	input := make([]string, 0)
 	readFile, err := os.Open(filename)
 	if err != nil {
@@ -108,7 +154,6 @@ func readFile() []string {
 
 func main() {
 	puzzle := readFile()
-	//fmt.Println(puzzle)
 	result := tailsPositions(puzzle)
 	fmt.Println(result)
 }
